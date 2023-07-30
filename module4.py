668f16d16e20951976ec7e2f1df45d77a41a50f3
Namit Mohale <33605285+namitmohale@users.noreply.github.com>
2023-07-29 22:01:46 -0700

    Update module4.py

--- module4.py
+++ module4.py
@@ -1,4 +1,7 @@
 def findIndex(inputNumbers, target):
+    # We loop over all the user provided N numbers and if we find the target number,
+    # we output index+1 (since indexes in computers start from 0 whereas we want them to start from 1),
+    # or we return -1 if we cannot find the target number.
     for i in range(inputNumbers):
         if inputNumbers[i] == target:
             return i + 1
@@ -6,16 +9,22 @@ def findIndex(inputNumbers, target):
 
 def main():
     N = 0
+    # Using a while loop to keep prompting the user until they input a valid value for N.
     while N <= 0:
         N = int(input("Enter the number of elements (N): "))
         if N <= 0:
             print("N must be a positive integer.")
+    
+    # inputNumbers will hold the N numbers provided by the user.
     inputNumbers = []
     for i in range(N):
         num = int(input(f"Enter number {i + 1}: "))
         inputNumbers.append(num)
+
+    # X is the number that we need to find among the numbers provided by the user.
     X = int(input("Enter the number to search (X): "))
 
+    # idx holds the final result.
     idx = findIndex(inputNumbers, X)
 
     if idx == -1:
