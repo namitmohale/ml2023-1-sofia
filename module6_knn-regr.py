cd6ab5d4b48238a3e4a6e82d238bbe16716effc7
Namit Mohale <33605285+namitmohale@users.noreply.github.com>
2023-08-12 18:41:50 -0700

    Add files via upload

---
+++ module6_knn-regr.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
+
+class KNNRegressor:
+    def __init__(self):
+        self.inputPoints = np.empty((0, 2))
+        self.k = 1
+    
+    def readPoints(self, n, k):
+        self.k = k
+        for i in range(n):
+            x = float(input(f"Enter x value for Point {i + 1}: "))
+            y = float(input(f"Enter y value for Point {i + 1}: "))
+            self.inputPoints = np.append(self.inputPoints, [[x, y]], axis=0)
+
+    def applyKNNAndPredit(self, inputX):
+        # Prepare the input for kNN.
+        X = self.inputPoints[:, 0].reshape(-1, 1)
+        y = self.inputPoints[:, 1]
+
+        knn_regressor = KNeighborsRegressor(n_neighbors=self.k)
+        knn_regressor.fit(X, y)
+
+        return knn_regressor.predict([[inputX]])[0]
+
+if __name__ == "__main__":
+    N = 0
+    while N <= 0:
+        N = int(input("Enter the number of points (N): "))
+        if N <= 0:
+            print("N must be a positive integer.")
+    
+    k = 0
+    while k <= 0 or k > N:
+        k = int(input("Enter number of neighbors for kNN (k): "))
+        if k <= 0 or k > N:
+            print("k must be a positive integer <= N")
+    
+    knnRegressor = KNNRegressor()
+    knnRegressor.readPoints(N, k)
+
+    inputX = float(input("Enter input X for prediction by kNN Regressor: "))
+
+    yPredicted = knnRegressor.applyKNNAndPredit(inputX)
+
+    print(f"Value of y predicted by kNN Regressor for input X ({inputX}): ", yPredicted)
